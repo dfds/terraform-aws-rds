f572a985d2426ab122f93b189eca3b1d1082b1c5
samidbb <samdi@dfds.com>
2023-12-11 11:56:12 +0100

    test region

--- tests/qa/cloudwatch_log_groups.sh
+++ tests/qa/cloudwatch_log_groups.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 RDS_INSTANCE_NAME=qa; export RDS_INSTANCE_NAME
+export AWS_DEFAULT_REGION=eu-central-1
 aws s3 ls
 # aws logs describe-log-groups --region eu-central-1 | jq -r '.logGroups[].logGroupName' | while read file
 # do
