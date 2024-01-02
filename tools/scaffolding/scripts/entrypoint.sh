b145a5761a17f87d533c7650b4115e077477bed3
samidbb <samdi@dfds.com>
2024-01-02 21:51:25 +0100

    checks and cleanup script

--- tools/scaffolding/scripts/entrypoint.sh
+++ tools/scaffolding/scripts/entrypoint.sh
@@ -1,7 +1,3 @@
-
-# SOURCE_MODULE_PATH="../../"
-# OUTPUT_FILE="$PWD/temp/doc.json"
-
 SCRIPTS_PATH="/scripts"
 SOURCE_MODULE_PATH="/input"
 
@@ -26,28 +22,19 @@ DOCKER_OUTPUT_FOLDERS="/output/docker"
 mkdir -p $DOCKER_OUTPUT_FOLDERS
 
 if [ -z "$(ls -A $SOURCE_MODULE_PATH)" ]; then
-   echo "Empty"
-else
-   echo "Not Empty"
+   echo "Empty $SOURCE_MODULE_PATH"
+   exit 1
 fi
 # TODO: CHECK FOR output folder mounted
-
-
-# terraform-docs json --show "all" $SOURCE_MODULE_PATH --output-file $OUTPUT_JSON_FILE
-
-# mkdir -p temp
 
 # 1) Generate docs for all modules in a repo
 terraform-docs json --show "all" $SOURCE_MODULE_PATH --output-file $OUTPUT_JSON_FILE
 
- # 2) Generate files
-# mkdir -p auto-generated/terraform
+# 2) Generate TF files
 python3 $SCRIPTS_PATH/generate_module.py $SOURCE_JSON_DOC $GENERATED_TF_MODULE_DATA $TF_MODULE_TEMPLATE $TF_MODULE_OUTPUT
 
-# mkdir -p auto-generated/docker
+# 3) Generate Docker files
 python3 $SCRIPTS_PATH/generate_docker.py $DOCKER_COMPOSE_TEMPLATE $DOCKER_COMPOSE_OUTPUT $DOCKER_ENV_TEMPLATE $DOCKER_ENV_OUTPUT $DOCKER_SCRIPT_TEMPLATE $DOCKER_SCRIPT_OUTPUT
 
-# mkdir -p auto-generated/pipeline
+# 4) Generate pipeline files
 # TODO: generate pipeline
-
-# rm -rf temp
