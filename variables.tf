02fdc25ed22d6f47a424990a1974abc719ce9539
samidbb <samdi@dfds.com>
2024-02-07 12:47:23 +0100

    environment specific default configs for cw logs

--- variables.tf
+++ variables.tf
@@ -497,10 +497,12 @@ variable "cloudwatch_log_group_retention_in_days" {
   description = <<EOF
     Specify the retention period in days for the CloudWatch logs.
     Valid Values: Number of days
-    Notes: .
+    Notes:
+    - If omitted, the default value is set to 7 days for production and 1 day for non-production environments.
+    - If set to 0, logs will be retained indefinitely.
 EOF
   type        = number
-  default     = 1
+  default     = -1
 }
 
 variable "cloudwatch_log_group_kms_key_id" {
