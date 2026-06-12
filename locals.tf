021d3dce01f531f86b4c9b1a0e9e4714236714db
rifisdfds <rifis@dfds.com>
2026-06-12 07:56:56 +0100

    chore: format document

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   ########################################################################
   # Parameter group
   ########################################################################
-  pg_major_version          = tonumber(split(".", local.engine_version)[0])
+  pg_major_version                = tonumber(split(".", local.engine_version)[0])
   create_db_parameter_group       = true
   parameter_group_family          = data.aws_rds_engine_version.engine_info.parameter_group_family
   parameter_group_use_name_prefix = true
